564e256dd0ba2e31122d6213d6c4776623697764
Marc Antoni <ocanomark@yahoo.com>
2018-06-08 21:33:23 +0800

    Create 02_lone_sum.py

---
+++ Python/Logic-2/02_lone_sum.py
@@ -0,0 +1,8 @@
+# Given 3 int values, a b c, return their sum. However, if one of the values
+# is the same as another of the values, it does not count towards the sum.
+def lone_sum(a, b, c):
+  sum = 0
+  if (a != b and a!= c): sum += a
+  if (b != a and b != c): sum += b
+  if (c != a and c != b): sum += c
+  return sum
