a5feffd878e6073512c8c7f677f1924b99b1696a
Marc Antoni <ocanomark@yahoo.com>
2018-06-08 21:46:23 +0800

    Create 06_close_far.py

---
+++ Python/Logic-2/06_close_far.py
@@ -0,0 +1,12 @@
+# Given three ints, a b c, return True if one of b or c is "close" (differing
+# from a by at most 1), while the other is "far", differing from both other
+# values by 2 or more. Note: abs(num) computes the absolute value of a number.
+def close_far(a, b, c):
+  return (is_close(a, b) and is_far(a, b, c)) or \
+  (is_close(a, c) and is_far(a, c, b))
+  
+def is_close(a, b):
+  return abs(a - b) <= 1
+  
+def is_far(a, b, c):
+  return abs(a - c) > 1 and abs(b - c) > 1
